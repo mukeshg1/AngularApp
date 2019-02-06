ab6386722f81195cd9292438738fdbbc4c60e5a1
Mukesh Gupta <mukeshg@mindfiresolutions.com>
2019-02-06 19:37:14 +0530

    End of the day commit.

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -23,14 +23,12 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {name: 'Amit', address: 'Banglore', dob: '2/1/2019', email: 'amit@email.com'},
-  {name: 'Birodh', address: 'Bhubaneswar', dob: '2/1/2019', email: 'birodh@email.com'},
-  {name: 'Chandra', address: 'Chennai', dob: '1/24/2019', email: 'chandra@email.com'},
-  {name: 'Dev', address: 'Kathmandu', dob: '1/24/2019', email: 'dev@email.com'},
-  {name: 'Ellen', address: 'Biratnagar', dob: '1/24/2019', email: 'ellen@email.com'},
-  {name: 'Forrest', address: 'Lahan', dob: '11/24/2019', email: 'forrest@email.com'},
-  {name: 'Gourav', address: 'Pokhara', dob: '1/24/2019', email: 'gourav@email.com'},
-  {name: 'Harry', address: 'Namche Bazar', dob: '1/24/2019', email: 'harry@email.com'},
-  {name: 'Irving', address: 'Boston', dob: '1/24/2019', email: 'irving@email.com'},
-  {name: 'Johnny', address: 'New Delhi', dob:'1/24/2019', email: 'johnny@email.com'},
+  {name: 'Amit Rastogi', address: 'Banglore', dob: '2/1/2019', email: 'amit@email.com'},
+  {name: 'Birodh Basnet', address: 'Bhubaneswar', dob: '2/1/2019', email: 'birodh@email.com'},
+  {name: 'Chandra Adhikari', address: 'Chennai', dob: '1/24/2019', email: 'chandra@email.com'},
+  {name: 'Devasis Das', address: 'Kathmandu', dob: '1/24/2019', email: 'dev@email.com'},
+  {name: 'Ellen DeGeneres', address: 'Biratnagar', dob: '1/24/2019', email: 'ellen@email.com'},
+  {name: 'Forrest Gump', address: 'Lahan', dob: '11/24/2019', email: 'forrest@email.com'},
+  {name: 'Gourav Padhi', address: 'Pokhara', dob: '1/24/2019', email: 'gourav@email.com'},
+  {name: 'Harry Kane', address: 'Namche Bazar', dob: '1/24/2019', email: 'harry@email.com'}
 ];
