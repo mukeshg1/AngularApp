6e6ede5fe197ca33787e2392d18dadff17740550
Mukesh Gupta <mukeshg@mindfiresolutions.com>
2019-02-11 16:58:17 +0530

    JSON File Updated.

--- src/app/Services/formdata.service.ts
+++ src/app/Services/formdata.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { formdata } from '../models/formdata';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormdataService {
 
-  constructor() { }
+  formdataUrl:string = 'https://my-json-server.typicode.com/mukeshg1/AngularApp/users';
+
+  constructor(private http:HttpClient) { }
+
+  //Get FormData
+
+  getFormdata():Observable<formdata[]>{
+    return this.http.get<formdata[]>(this.formdataUrl);
+  }
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormComponent } from '../form/form.component';
+import { FormdataService } from '../services/formdata.service';
+import { formdata } from '../models/formdata';
 
 @Component({
   selector: 'app-table',
@@ -8,30 +9,18 @@ import { FormComponent } from '../form/form.component';
 })
 export class TableComponent implements OnInit {
 
-  constructor(private _formData: FormComponent) { 
+  FormData: formdata[];
+
+  constructor(private formdataService:FormdataService) { 
     
   }
 
   ngOnInit() {
+
+    this.formdataService.getFormdata().subscribe(FormData => {
+      this.FormData = FormData;
+    })
   }
   displayedColumns = ['name', 'address', 'dob', 'email'];
-  dataSource = ELEMENT_DATA;
 
 }
-export interface FormData {
-  address: string;
-  name: string;
-  dob: string;
-  email: string;
-}
-
-const ELEMENT_DATA: FormData[] = [
-  {name: 'Amit Rastogi', address: 'Banglore', dob: '2/1/2019', email: 'amit@email.com'},
-  {name: 'Birodh Basnet', address: 'Bhubaneswar', dob: '2/1/2019', email: 'birodh@email.com'},
-  {name: 'Chandra Adhikari', address: 'Chennai', dob: '1/24/2019', email: 'chandra@email.com'},
-  {name: 'Devasis Das', address: 'Kathmandu', dob: '1/24/2019', email: 'dev@email.com'},
-  {name: 'Ellen DeGeneres', address: 'Biratnagar', dob: '1/24/2019', email: 'ellen@email.com'},
-  {name: 'Forrest Gump', address: 'Lahan', dob: '11/24/2019', email: 'forrest@email.com'},
-  {name: 'Gourav Padhi', address: 'Pokhara', dob: '1/24/2019', email: 'gourav@email.com'},
-  {name: 'Harry Kane', address: 'Namche Bazar', dob: '1/24/2019', email: 'harry@email.com'}
-];
